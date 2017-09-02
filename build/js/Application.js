f28122d40693609955bb8c0a7b637c66af4969f8
TOMATONO-WIN10\tomat <tomatono@gmail.com>
2017-09-03 01:57:57 +0900

    webpackでjsファイルを1つにまとめる

---
+++ build/js/Application.js
@@ -0,0 +1,10 @@
+export class Application {
+    constructor() {
+        this.text1 = "";
+        let x = $("a");
+        // ko.track;
+        // $("").;
+        // ko
+    }
+}
+//# sourceMappingURL=Application.js.map
---
+++ src/ts/views/Application.ts
@@ -0,0 +1,15 @@
+
+export class Application {
+    text1: string;
+
+    constructor() {
+        this.text1 = "";
+
+
+        let x: JQuery  = $("a");
+
+        // ko.track;
+        // $("").;
+        // ko
+    }
+}
